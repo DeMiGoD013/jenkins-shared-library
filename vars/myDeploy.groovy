38a68cb5aed9b65a9dd25cfe2ce542de68323a33
Sai Prasad Padmanabha <108669588+DeMiGoD013@users.noreply.github.com>
2025-12-06 11:21:14 +0530

    Update myDeploy.groovy

--- vars/myDeploy.groovy
+++ vars/myDeploy.groovy
@@ -1,13 +1,21 @@
 def call(manifestPath, kubeCredId) {
+
     withCredentials([file(credentialsId: kubeCredId, variable: 'KCFG')]) {
 
         sh """
-        export KUBECONFIG=\$KCFG
+        export KUBECONFIG=${KCFG}
 
-        sed 's|REPLACE_IMAGE|${env.IMAGE}|g' ${manifestPath} > /tmp/apply.yaml
+        # Replace image in YAML
+        sed "s|REPLACE_IMAGE|${env.IMAGE}|g" ${manifestPath} > /tmp/apply.yaml
 
+        # Apply manifest
         kubectl apply -f /tmp/apply.yaml
-        kubectl rollout status deployment/$(grep -m1 'name:' ${manifestPath} | awk '{print $2}') --timeout=120s
+
+        # Extract deployment name automatically
+        DEPLOY_NAME=\$(grep -m1 'name:' ${manifestPath} | awk '{print \$2}')
+
+        # Rollout status
+        kubectl rollout status deployment/\$DEPLOY_NAME --timeout=120s
         """
     }
 }
