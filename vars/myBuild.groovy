e72ec21d97b1ba5781825c388cae17d76ec11dd5
Sai Prasad Padmanabha <108669588+DeMiGoD013@users.noreply.github.com>
2025-12-08 15:10:10 +0530

    Update myBuild.groovy

--- vars/myBuild.groovy
+++ vars/myBuild.groovy
@@ -1,7 +1,6 @@
 def call() {
     echo "🔧 Shared Library: myBuild() function is running"
     echo "Shared Library Updated Successfully!"
-    echo "This automatically pulling the code"
     echo "Building image: ${env.IMAGE}"
     sh "podman build -t ${env.IMAGE} ."
 }
